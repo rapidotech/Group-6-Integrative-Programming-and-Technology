5b913f51dc662b208e3d86636a6f509c044e10bc
rapidotech <161102302+rapidotech@users.noreply.github.com>
2025-03-14 08:30:51 +0800

    Delete connectly_project directory

--- connectly_project/posts/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsPostAuthor(BasePermission):
-    """
-    Permission that allows only the author of a post to view or edit it.
-    """
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
