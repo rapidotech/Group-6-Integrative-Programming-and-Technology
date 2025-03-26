179e74c2f5d75d19cb2105579788ec0119754742
rapidotech <161102302+rapidotech@users.noreply.github.com>
2025-03-26 16:25:37 +0800

    Delete connectly_project directory

--- connectly_project/connectly_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from posts.views import LoginView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls')),
-    path('login/', LoginView.as_view(), name='login'),
-]
